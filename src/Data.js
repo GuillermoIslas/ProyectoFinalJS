baf67b911cd5444dcd7e5d561432f19e017070fe
Guillermo Islas <guilleislas2001@gmail.com>
2022-09-24 22:31:15 -0300

    Entrega del proyecto final

---
+++ src/Data.js
@@ -0,0 +1,25 @@
+let shopItemsData = [{
+    id:"jksfjkdfj",
+    name:"Panel solar",
+    price: 1200,
+    desc: "Panel solar para generar electricidad",
+    img:"img/panel.png",
+},{
+    id:"gjfhgifhg",
+    name:"Colector solar",
+    price: 1000,
+    desc: "Colector solar para agua caliente sanitaria",
+    img: "img/colector.webp",
+},{
+    id:"ejitueiwu",
+    name:"Bomba de calor",
+    price: 1700,
+    desc: "Bomba de calor para climatización",
+    img: "img/bomba.png",
+},{
+    id:"mvncvnjvbd",
+    name:"Aislamiento térmico",
+    price: 2300,
+    desc: "Aislamiento térmico para mejorar el confort térmico",
+    img: "img/aislamiento.jpg",
+}]
